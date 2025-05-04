7a704d0dc385a84b17ce886ea52b6807b93e53cc
Thomas <thomascode92@hotmail.com>
2025-05-04 16:27:19 +0200

    ESLint and Prettier Setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(
   // Your custom configs here
-)
+  {
+    plugins: { prettier: eslintPluginPrettier },
+    rules: { "prettier/prettier": "error" },
+  },
+);
