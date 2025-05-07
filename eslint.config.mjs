b0349def02968f25026faa1c38d281ae98853806
Thomas <thomascode92@hotmail.com>
2025-05-07 17:09:19 +0200

    Simplify ESLint Config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,9 @@
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
 // @ts-check
-import eslintPluginPrettier from "eslint-plugin-prettier";
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(
   // Your custom configs here
-  {
-    plugins: { prettier: eslintPluginPrettier },
-    rules: { "prettier/prettier": "error" },
-  },
+  eslintPluginPrettierRecommended,
 );
