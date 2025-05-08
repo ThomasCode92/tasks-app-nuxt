0780da9b156df991c24a618d335e714e0b04722e
Thomas <thomascode92@hotmail.com>
2025-05-08 17:13:05 +0200

    Validating Data with 'readValidatedBody'

--- server/api/tasks.post.ts
+++ server/api/tasks.post.ts
@@ -1,1 +1,15 @@
-export default defineEventHandler(async () => {});
+import { InsertTasksSchema } from "~/lib/db/schema";
+
+export default defineEventHandler(async event => {
+  const result = await readValidatedBody(event, InsertTasksSchema.safeParse);
+
+  if (!result.success) {
+    const error = createError({
+      statusCode: 422,
+      statusMessage: "Invalid task",
+    });
+    return sendError(event, error);
+  }
+
+  return { task: result.data };
+});
