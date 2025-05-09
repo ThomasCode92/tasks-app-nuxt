55d2eeac17fac0b7a00cb0d2fb3cfad33f699172
Thomas <thomascode92@hotmail.com>
2025-05-09 07:37:43 +0200

    Submitting a Form with 'fetch'

--- pages/create.vue
+++ pages/create.vue
@@ -1,10 +1,43 @@
+<script setup lang="ts">
+import type { FetchError } from "ofetch";
+
+const errorMessage = ref("");
+const isLoading = ref(false);
+const taskName = ref("");
+
+async function onSubmit() {
+  if (!taskName.value.trim()) {
+    errorMessage.value = "Task name is required";
+    return;
+  }
+
+  try {
+    isLoading.value = true;
+    errorMessage.value = "";
+
+    const result = await $fetch("/api/tasks", {
+      method: "POST",
+      body: { title: taskName.value },
+    });
+    console.log(result);
+  } catch (e) {
+    const error = e as FetchError;
+    errorMessage.value = error.statusMessage ?? "Unknown error occurred";
+  }
+
+  isLoading.value = false;
+}
+</script>
+
 <template>
   <div>
     <h1>Create Task</h1>
-    <form>
+    <article v-if="isLoading" aria-busy="true" />
+    <article v-else-if="errorMessage" class="error">{{ errorMessage }}</article>
+    <form @submit.prevent="onSubmit">
       <label>
         Task
-        <input type="text" name="title" />
+        <input v-model="taskName" type="text" name="title" />
       </label>
       <div class="button-container">
         <button>Create</button>
