8ce918e6802636a5046bc7706999e12a6047f4a3
Thomas <thomascode92@hotmail.com>
2025-05-10 20:31:12 +0200

    Get Route Params with 'useRoute'

--- pages/tasks/[id].vue
+++ pages/tasks/[id].vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
-const { data, status, error } = await useFetch(`/api/tasks/${}`, { lazy: true });
+const route = useRoute();
+const { data, status, error } = await useFetch(
+  `/api/tasks/${route.params.id}`,
+  { lazy: true },
+);
 </script>
 
 <template>
-  <h1>The Single Task Page</h1>
+  <article v-if="status === 'pending'" aria-busy="true" />
+  <article v-else-if="error" class="error">{{ error.statusMessage }}</article>
+  <article v-else-if="data">
+    <h1>{{ data.task.title }}</h1>
+  </article>
 </template>
